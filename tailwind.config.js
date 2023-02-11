cbf009b759b2fac47a2ba43f67c8d827fd018a75
MrNdee <nnamdionah@gmail.com>
2023-02-11 19:59:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    extend: {
+      colors: {
+        fineYellow: '#FFB703',
+        orangeYellow: '#FB8500',
+        strongCyan: '#219EBC',
+        verySoftBlue: '#8ECAE6',
+        veryDarkBlue: '#023047',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+}
